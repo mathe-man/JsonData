54aa942f4e144ae4635c5bbb6ebe76517e0964d4
mathe-man <mathieumousset6@gmail.com>
2025-07-10 08:43:05 +0300

    Add solution files

---
+++ JsonData/JsonManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace JsonData;
+
+
+
+public class JsonManager
+{
+    private JsonSerializerSettings _settings = new JsonSerializerSettings
+    {
+        Formatting = Formatting.Indented,
+        ContractResolver = new DefaultContractResolver()
+        {
+            DefaultMembersSearchFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance,
+        }
+    };
+
+    public string DefaultFileExtension { get; set; } = ".json";
+
+    private Dictionary<string, List<object>> _objectToSave = new Dictionary<string, List<object>>();
+
+    private void SafeKey(string key)
+    {
+	    if (!_objectToSave.ContainsKey(key))
+		    _objectToSave.Add(key, new List<object>());
+    }
+    
+    public void AddObjectToSave(object obj, string path)
+    {
+	    SafeKey(path);
+        _objectToSave[path].Add(obj);
+    }
+
+    private void SavePath(string path)
+    {
+	    SafeKey(path);
+	    string json = "";
+	    
+	    json += JsonConvert.SerializeObject(_objectToSave[path], _settings);
+	    
+	    if (!Path.HasExtension(path))
+		    path += ".json";
+	    
+	    
+	    File.WriteAllText(path, json);
+	    Console.WriteLine($"Saved the {path} file");
+    }
+    public void Save(string? path = null)
+    {
+	    if (path == null)
+		    foreach (var obj in _objectToSave)
+			    SavePath(obj.Key);
+
+	    else
+	    {
+		    SafeKey(path);
+		    SavePath(path);
+	    }
+    }
+}
---
+++ LibTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using JsonData;
+
+
+class Person
+{
+    public string name          { get; set; }
+    private string password;
+    public Dictionary<string, List<string>> favorites { get; set; }
+    public Person? parent   { get; set; }
+
+    public Person(string name, string password, Person? parent = null)
+    {
+	    this.name = name;
+	    this.password = password;
+	    this.parent = parent;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        JsonManager jm = new JsonManager();
+        var Bea = new Person("Bea", "123456");
+        
+        Bea.favorites = new Dictionary<string, List<string>>()
+        {
+	        {"Some names", ["Alfred", "John" ]},
+	        {"Some foods", ["Bean"  , "Pizza"]}
+        };
+        var Fred = new Person("Fred", "azertyuiop");
+
+        Bea.parent = Fred;
+        
+        jm.AddObjectToSave(Bea, "../Person.dat");
+        jm.AddObjectToSave(Fred, "../Person.dat");
+        jm.Save();
+    }
+}
